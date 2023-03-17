640608fdaf41d5a78f2d5f22b31c078270f4920f
habsatou97 <103775782+habsatou97@users.noreply.github.com>
2023-03-17 11:35:03 +0000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import{ EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
-
+import { NavBarComponent } from './nav/nav-bar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -1,31 +1,41 @@
-import { Component,Input,Output,EventEmitter } from "@angular/core";
+import { style } from "@angular/animations";
+import { Component,Input } from "@angular/core";
 
 @Component({
     selector: 'event-thumbnail',
     template:`
     <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div> Date:{{event.date}} </div>
-      <div> Time: {{event.time}}</div>
-      <div> Price: {{event.price}}\FCFA</div>
-      <div>
-          <span> Location:{{event.location.address}}</span>
-          <span> &nbsp; </span>
-          <span> {{event.location.city}},{{event.location.country}}</span>
+      <h2>{{event?.name}}</h2>
+      <div> Date:{{event?.date}} </div>
+      <div> Time: {{event?.time}}</div>
+      <div [ngSwitch]="event?.time">
+      <span *ngSwitchCase=" '8:00 am'">Early Start</span>
+      <span *ngSwitchCase=" '9:00 am'">Normal Start</span>
+      <span *ngSwitchCase=" '10:00 am'">Late Start</span>
       </div>
-      <button class="btn btn-primary" (click)="handleClickMe()"> Click Me!</button>
+      <div> Price: {{event?.price}}\FCFA</div>
+      <div>
+          <span> Location:{{event?.location.address}}</span>
+          <span class="pad-left"> {{event?.location.city}},{{event?.location.country}}</span>
+      </div>
     </div>
-    `
+    `,
+    styles:[`
+           .thumbnail {min-height: 210px;}
+           .pad-left{margin-left: 10px;}
+           .well { color:#bbb}
+    
+          `]
 
 })
 
 export class EventThumbnailComponent{
    @Input() event:any
-   @Output() eventClick= new EventEmitter()
+   someProperty:any ="Habsatou"
 
-   handleClickMe()
+   logFoo()
    {
-    this.eventClick.emit("clicked");
+    console.log("foo");
    }
 
 }
